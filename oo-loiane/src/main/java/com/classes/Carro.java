7505e2fca74540c98c7335256f17113b83246409
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2022-03-21 21:30:14 -0300

    metodo simples

---
+++ oo-loiane/src/main/java/com/classes/Carro.java
@@ -0,0 +1,17 @@
+package com.classes;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	void exibirAutonomia() {
+		System.out.println("A autonomia do carro é " + capCombustivel * consumoCombustivel + " km");
+		
+		
+	}
+	
+}
