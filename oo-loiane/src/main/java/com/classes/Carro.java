9595c954633c010d5b81f60a42f73bd5a5ddf847
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2022-03-22 21:23:17 -0300

    criando getters e setters

--- oo-loiane/src/main/java/com/classes/Carro.java
+++ oo-loiane/src/main/java/com/classes/Carro.java
@@ -2,11 +2,11 @@ package com.classes;
 
 public class Carro {
 	
-	String marca;
-	String modelo;
-	int numPassageiros;
-	double capCombustivel;
-	double consumoCombustivel;
+	private String marca;
+	private String modelo;
+	private int numPassageiros;
+	private double capCombustivel;
+	private double consumoCombustivel;
 	
 	
 	public Carro() {
@@ -36,4 +36,48 @@ public class Carro {
 		double qtdCombustivel = km/this.consumoCombustivel;
 		return qtdCombustivel;
 	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getNumPassageiros() {
+		return numPassageiros;
+	}
+
+	public void setNumPassageiros(int numPassageiros) {
+		this.numPassageiros = numPassageiros;
+	}
+
+	public double getCapCombustivel() {
+		return capCombustivel;
+	}
+
+	public void setCapCombustivel(double capCombustivel) {
+		this.capCombustivel = capCombustivel;
+	}
+
+	public double getConsumoCombustivel() {
+		return consumoCombustivel;
+	}
+
+	public void setConsumoCombustivel(double consumoCombustivel) {
+		this.consumoCombustivel = consumoCombustivel;
+	}
+	
+	
+	
+	
 }
--- oo-loiane/src/main/java/com/classes/TesteCarro.java
+++ oo-loiane/src/main/java/com/classes/TesteCarro.java
@@ -1,10 +1,20 @@
 package com.classes;
 
+
+
 public class TesteCarro {
 
 	public static void main(String[] args) {
 		
 		Carro van = new Carro("Fiat", "Ducato", 10, 100, 0.2);
+		
+		Carro saveiro = new Carro();
+		
+		saveiro.setMarca("Volkswagen");
+		saveiro.setModelo("Saveiro");
+		saveiro.setNumPassageiros(2);
+		saveiro.setCapCombustivel(120);
+		saveiro.setConsumoCombustivel(0.3);
 		
 		 
 		van.exibirAutonomia();
