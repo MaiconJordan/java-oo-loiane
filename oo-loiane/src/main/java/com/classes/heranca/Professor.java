3d6d4cb127d4fe53f66580b165ec37b4465061b8
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2022-03-23 21:01:01 -0300

    Construtor Super Classe

--- oo-loiane/src/main/java/com/classes/heranca/Professor.java
+++ oo-loiane/src/main/java/com/classes/heranca/Professor.java
@@ -1,5 +1,28 @@
 package com.classes.heranca;
 
-public class Professor {
+public class Professor extends Pessoa {
+	
+	
+	private double salario;
+	private String nomeCurso;
+
+	
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public double calcularSalarioLiquido() {
+		return 0;
+	}
 
 }
--- oo-loiane/src/main/java/com/classes/heranca/teste.java
+++ oo-loiane/src/main/java/com/classes/heranca/teste.java
@@ -1,5 +1,15 @@
 package com.classes.heranca;
 
 public class teste {
-
+	
+	public static void main (String[] args) {
+		
+	
+		Aluno aluno = new Aluno();
+		
+		aluno.setNome("nome");
+		
+		System.out.println(aluno.getNome());
+	}	
+	
 }
