41b967c3a0f158be63529ebfc3d1c10f21918e0d
Maicon Jordan <maiconplus@gmail.com>
2024-07-22 21:39:11 -0300

    metodo simples

---
+++ oo-loiane/src/main/java/com/carro/Carro.java
@@ -0,0 +1,16 @@
+package com.carro;
+
+public class Carro {
+	
+	 String marca;
+	 String tipo;
+	 String combustivel;
+	 int anoModelo;
+	
+	public void informarcoesCarro() {
+		System.out.println(marca + " " + tipo + " " + combustivel + " " + anoModelo);
+	}
+	
+	
+
+}
---
+++ oo-loiane/src/main/java/com/carro/TesteCarro.java
@@ -0,0 +1,19 @@
+package com.carro;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro carro1 = new Carro();
+		
+		carro1.marca = "VW";
+		carro1.tipo = "Hatch";
+		carro1.combustivel = "Gasolina";
+		carro1.anoModelo = 98;
+		
+		// Metodo Simples
+		carro1.informarcoesCarro();
+
+	}
+
+}
