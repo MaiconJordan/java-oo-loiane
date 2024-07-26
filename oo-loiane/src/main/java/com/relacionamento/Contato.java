029c87c9d23da40174b73f258fe125bfaf5b9e93
Maicon Jordan <maiconplus@gmail.com>
2024-07-25 23:05:54 -0300

    classe contato

---
+++ oo-loiane/src/main/java/com/relacionamento/Contato.java
@@ -0,0 +1,31 @@
+package com.relacionamento;
+
+public class Contato {
+	private String nome;
+	private Endereco endereco;
+	private String Telefone;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return Telefone;
+	}
+	public void setTelefone(String telefone) {
+		Telefone = telefone;
+	}
+	
+	
+
+}
